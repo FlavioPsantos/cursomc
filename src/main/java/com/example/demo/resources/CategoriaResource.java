948382096ca45bc1be3c99258fadc879fc3383d1
Flavio Santos <flavioraph@live.com>
2021-05-15 16:57:39 -0300

    Classe Categoria Iniciada

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,15 +1,30 @@
 package com.example.demo.resources;
 
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.CategoriaDomain;
+
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<CategoriaDomain> listar() {
+		CategoriaDomain cat1 = new CategoriaDomain(1, "informática");
+		CategoriaDomain cat2 = new CategoriaDomain(2, "Escritório");
+		
+		List<CategoriaDomain> listaDeCategorias = new ArrayList<>();
+		listaDeCategorias.add(cat1);
+		listaDeCategorias.add(cat2);
+		
+		return listaDeCategorias;
 	}
 }
