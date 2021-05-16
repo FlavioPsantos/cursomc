eb9f9ac62b6ae6dff2c45efcc31123f3ce2ff3bc
Flavio Santos <flavioraph@live.com>
2021-05-16 15:12:53 -0300

    Implementando Auto Insert Categorias

--- src/main/java/com/example/demo/MyfirstprojectApplication.java
+++ src/main/java/com/example/demo/MyfirstprojectApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MyfirstprojectApplication {
+import com.example.demo.domain.CategoriaDomain;
+import com.example.demo.repositorie.CategoriaRepository;
 
+@SpringBootApplication
+public class MyfirstprojectApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		CategoriaDomain cat1 = new CategoriaDomain(null, "informática");
+		CategoriaDomain cat2 = new CategoriaDomain(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
