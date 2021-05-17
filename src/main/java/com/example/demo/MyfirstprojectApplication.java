f7731b4fca26990eb827c367b1fab508bc374cc5
Flavio Santos <flavioraph@live.com>
2021-05-17 18:25:05 -0300

    Criação de Estado e Cidade

--- src/main/java/com/example/demo/MyfirstprojectApplication.java
+++ src/main/java/com/example/demo/MyfirstprojectApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.domain.CategoriaDomain;
+import com.example.demo.domain.Cidade;
+import com.example.demo.domain.Estado;
 import com.example.demo.domain.Produto;
 import com.example.demo.repositorie.CategoriaRepository;
+import com.example.demo.repositorie.CidadeRepository;
+import com.example.demo.repositorie.EstadoRepository;
 import com.example.demo.repositorie.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class MyfirstprojectApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstprojectApplication.class, args);
@@ -42,8 +50,24 @@ public class MyfirstprojectApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		
+		
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 
