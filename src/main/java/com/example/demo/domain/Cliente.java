78c6a4882ce5d5374bdf15b2cc7dfd2e528a9773
Flavio Santos <flavioraph@live.com>
2021-05-18 20:21:56 -0300

    Atualização da JsonIgnore Json

--- src/main/java/com/example/demo/domain/Cliente.java
+++ src/main/java/com/example/demo/domain/Cliente.java
@@ -15,7 +15,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 public class Cliente  implements Serializable{
 	
@@ -28,7 +27,6 @@ public class Cliente  implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> endereços = new ArrayList<>();
 	
