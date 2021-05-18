d22ade4a56a95ecce949532a62217f3c00afeebc
Flavio Santos <flavioraph@live.com>
2021-05-17 22:30:58 -0300

    Telefones, Endereço, Cliente. Usando Enum com tratamento de locação de memória

--- src/main/java/com/example/demo/MyfirstprojectApplication.java
+++ src/main/java/com/example/demo/MyfirstprojectApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.domain.CategoriaDomain;
 import com.example.demo.domain.Cidade;
+import com.example.demo.domain.Cliente;
+import com.example.demo.domain.Endereco;
 import com.example.demo.domain.Estado;
 import com.example.demo.domain.Produto;
+import com.example.demo.domain.TipoCliente;
 import com.example.demo.repositorie.CategoriaRepository;
 import com.example.demo.repositorie.CidadeRepository;
+import com.example.demo.repositorie.ClienteRepository;
+import com.example.demo.repositorie.EnderecoRepository;
 import com.example.demo.repositorie.EstadoRepository;
 import com.example.demo.repositorie.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class MyfirstprojectApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstprojectApplication.class, args);
@@ -60,14 +69,18 @@ public class MyfirstprojectApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
-		
-		
-		
+		Cliente cli1 = new Cliente(null,"Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("32781456", "65478963"));
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220734", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "150", "Sala 800", "Centro", "38777012", cli1, c2);
+		cli1.getEndereços().addAll(Arrays.asList(e1,e2));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
 	}
 
---
+++ src/main/java/com/example/demo/repositorie/EnderecoRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repositorie;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.example.demo.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends  JpaRepository<Endereco, Integer>{
+	
+}
+
